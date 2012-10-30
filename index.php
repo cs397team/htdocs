fa6d5fe9b53015d0548ad82f87b55285a8c14e66
Andrew Schrader <arstk8@mst.edu>
2012-10-30 18:18:09 -0500

    Adding the base htdocs files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	//header('Location: '.$uri.'/xampp/');
+
+        echo "<h1>Hello World!</h1>";
+	echo "<p>Click <a href=\"/xampp\">Here</a> for some XAMPP Info</p>";
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
