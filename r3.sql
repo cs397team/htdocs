308f994c4fdef57c06be2bbeefb668ab6ac559b7
Andrew Schrader <arstk8@mst.edu>
2012-11-01 19:04:41 -0500

    Changed foreign key constraints to 'cascade'.  Removed test data from tables.

--- r3.sql
+++ r3.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2012 at 11:25 PM
+-- Generation Time: Nov 02, 2012 at 01:03 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -35,13 +35,6 @@ CREATE TABLE IF NOT EXISTS `building` (
   KEY `campusName` (`campusName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `building`
---
-
-INSERT INTO `building` (`campusName`, `latitude`, `longitude`, `name`) VALUES
-('Missouri S&T', 1212, 2111, 'pimp room');
-
 -- --------------------------------------------------------
 
 --
@@ -55,13 +48,6 @@ CREATE TABLE IF NOT EXISTS `campus` (
   PRIMARY KEY (`campusName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `campus`
---
-
-INSERT INTO `campus` (`campusName`, `longitude`, `latitude`) VALUES
-('Missouri S&T', 2.3, 5);
-
 -- --------------------------------------------------------
 
 --
@@ -74,13 +60,6 @@ CREATE TABLE IF NOT EXISTS `department` (
   PRIMARY KEY (`Name`),
   KEY `Name` (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `department`
---
-
-INSERT INTO `department` (`Name`, `ChairID`) VALUES
-('Computer Science', 5);
 
 -- --------------------------------------------------------
 
@@ -106,13 +85,6 @@ CREATE TABLE IF NOT EXISTS `event` (
   `typeOfEvent` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `event`
---
-
-INSERT INTO `event` (`id`, `title`, `eventTimeStart`, `eventTimeEnd`, `accessTimeStart`, `accessTimeEnd`, `date`, `numAttendees`, `decorations`, `alcohol`, `prizes`, `tickets`, `outsideVendors`, `foodOption`, `typeOfEvent`) VALUES
-(1, 'Hello World', '2012-10-17 08:20:28', '2012-10-25 11:10:10', '2012-10-27 10:24:31', '2012-10-31 13:16:20', '2012-10-24', 1337, 1, 1, 1, 0, NULL, 0, 'Saying hello to the world');
 
 -- --------------------------------------------------------
 
@@ -142,13 +114,6 @@ CREATE TABLE IF NOT EXISTS `organization` (
   KEY `department` (`department`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `organization`
---
-
-INSERT INTO `organization` (`Name`, `department`, `description`) VALUES
-('IEEE', 'Computer Science', 'stuff and things');
-
 -- --------------------------------------------------------
 
 --
@@ -167,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   KEY `eventId` (`eventId`),
   KEY `primaryRoomNumber` (`primaryRoomNumber`),
   KEY `backupRoomNumber` (`backupRoomNumber`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -186,13 +151,6 @@ CREATE TABLE IF NOT EXISTS `room` (
   KEY `buildingName` (`buildingName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `room`
---
-
-INSERT INTO `room` (`buildingName`, `roomNumber`, `capacity`, `roomName`, `type`) VALUES
-('pimp room', 1337, 1337, 'not pimp room', 'not pimp room');
-
 -- --------------------------------------------------------
 
 --
@@ -208,13 +166,6 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`ID`, `Name`, `Email`, `isAdmin`) VALUES
-(12269597, 'Bob white', 'bw123@mst.edu', 1);
-
---
 -- Constraints for dumped tables
 --
 
@@ -222,35 +173,35 @@ INSERT INTO `user` (`ID`, `Name`, `Email`, `isAdmin`) VALUES
 -- Constraints for table `building`
 --
 ALTER TABLE `building`
-  ADD CONSTRAINT `building_ibfk_1` FOREIGN KEY (`campusName`) REFERENCES `campus` (`campusName`);
+  ADD CONSTRAINT `building_ibfk_4` FOREIGN KEY (`campusName`) REFERENCES `campus` (`campusName`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `member_of`
 --
 ALTER TABLE `member_of`
-  ADD CONSTRAINT `member_of_ibfk_2` FOREIGN KEY (`org_name`) REFERENCES `organization` (`Name`),
-  ADD CONSTRAINT `member_of_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`ID`);
+  ADD CONSTRAINT `member_of_ibfk_4` FOREIGN KEY (`org_name`) REFERENCES `organization` (`Name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `member_of_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `organization`
 --
 ALTER TABLE `organization`
-  ADD CONSTRAINT `organization_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`Name`);
+  ADD CONSTRAINT `organization_ibfk_2` FOREIGN KEY (`department`) REFERENCES `department` (`Name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`ID`),
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`),
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`primaryRoomNumber`) REFERENCES `room` (`roomNumber`),
-  ADD CONSTRAINT `reservation_ibfk_4` FOREIGN KEY (`backupRoomNumber`) REFERENCES `room` (`roomNumber`);
+  ADD CONSTRAINT `reservation_ibfk_8` FOREIGN KEY (`backupRoomNumber`) REFERENCES `room` (`roomNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_5` FOREIGN KEY (`user`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_6` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_ibfk_7` FOREIGN KEY (`primaryRoomNumber`) REFERENCES `room` (`roomNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `room`
 --
 ALTER TABLE `room`
-  ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`buildingName`) REFERENCES `building` (`name`);
+  ADD CONSTRAINT `room_ibfk_2` FOREIGN KEY (`buildingName`) REFERENCES `building` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
