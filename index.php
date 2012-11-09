1cb21ab7f52a168a84f6cb0ee0d5f2d3bdb039ba
Andrew Schrader <arstk8@mst.edu>
2012-11-09 10:38:17 -0600

    Updated index.php to do something somewhat useful

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	//header('Location: '.$uri.'/xampp/');
+<html>
+<title>R3 Homepage</title>
 
-        echo "<h1>Hello World!</h1>";
-	echo "<p>Click <a href=\"/xampp\">Here</a> for some XAMPP Info</p>";
-	exit;
+<head>
+<h1>Welcome to R3 Room Reservation System!</h1>
+</head>
+
+<body>
+<hr />
+<?php
+if($_SERVER['SERVER_PORT'] != '443') 
+{ 
+    header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+}
+
+//Start session
+session_start();
+	
+//Check whether the session variable SESS_MEMBER_ID is present or not
+if(!isset($_SESSION['SESS_STUDENT_ID']) || (trim($_SESSION['SESS_STUDENT_ID']) == '')) {
+	echo "<p>You are currently not logged in.</p>";
+    echo "<p>Click <a href=\"login.php\">here</a> to get logged in and access the rest of the system.</p>";
+} else {
+    echo "<p>You are currently logged in</p>";
+    echo "<p>Click <a href=\"member-index.php\">here</a> to go to the member area.</p>";
+    echo "<p>Click <a href=\"logout.php\">here</a> to logout.</p>";
+}
 ?>
-Something is wrong with the XAMPP installation :-(
+</body>
+</html>
