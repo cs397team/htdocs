c0324ceb8fb18054429e082dc17bfdfd27539266
Andrew Schrader <arstk8@mst.edu>
2012-11-29 14:36:59 -0600

    Made it so index redirects the user to the right place.

--- index.php
+++ index.php
@@ -17,13 +17,20 @@ if($_SERVER['SERVER_PORT'] != '443')
 session_start();
 	
 //Check whether the session variable SESS_MEMBER_ID is present or not
-if(!isset($_SESSION['SESS_STUDENT_ID']) || (trim($_SESSION['SESS_STUDENT_ID']) == '')) {
-	echo "<p>You are currently not logged in.</p>";
-    echo "<p>Click <a href=\"login.php\">here</a> to get logged in and access the rest of the system.</p>";
-} else {
-    echo "<p>You are currently logged in</p>";
-    echo "<p>Click <a href=\"member-index.php\">here</a> to go to the member area.</p>";
-    echo "<p>Click <a href=\"logout.php\">here</a> to logout.</p>";
+if(!isset($_SESSION['SESS_STUDENT_ID']) || (trim($_SESSION['SESS_STUDENT_ID']) == '')) 
+{
+    header("location: login.php");
+} 
+else
+{
+    if($_SESSION['SESS_ISADMIN'] == 0)
+    {
+        header("location: member-index.php");
+    }
+    else
+    {
+        header("location: admin-index.php");
+    }
 }
 ?>
 </body>
